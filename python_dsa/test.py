b2eb02e230ca4ade20efb1f52a57784d08679eac
user07om <omkarbulbule7@gmail.com>
2025-01-21 01:15:31 +0530

    just the test for dump scripts

--- python_dsa/test.py
+++ python_dsa/test.py
@@ -10,23 +10,49 @@ def ptrn(n):
 def ptrn1(n):
     for i in range(n):
         # DOWN TRIANGLE
-        for j in range(n-i+1):
+        for j in range(n-i):
             print(" ", end=" ")
-        for j in range(2*i):
+        for j in range(2*i-1):
             print("*", end=" ")
         print()
     # print() 
-    # for i in range(n):
-    #     # UP TRIANGLE
-    #     for j in range(i):
-    #         print(" ", end=" ")
-    #     for j in range(2*n-(2*i)):
-    #         print("*", end=" ")
-    #     print()
+    for i in range(n):
+        # UP TRIANGLE
+        for j in range(i):
+            print(" ", end=" ")
+        for j in range(2*n-(2*i+1)):
+            print("*", end=" ")
+        print()
+
+def ptrn3(n):
+    for i in range(2*n):
+        start = i
+        if i >= n: start = 2*n-i
+        for _ in range(start):
+            print("*", end=" ")
+        print()
+
+
+def ptrn4(n):
+    for i in range(1, n+1):
+        for _ in range(n-i):
+            print("-", end=" ")
+        letter = ord('A')
+        for j in range(2*i-2):
+            print(chr(letter), end=" ")
+            if j<=(2*i)/2: letter+=1
+            else: letter-=3
+        print()
+    # for i in range(1, n+1):
+        # if i%2==0: counter = 1
+        # else: counter = 0
+
+
+    
 
 
 
 #def ptrn1(n):
 # ptrn(4)
-ptrn1(4)
+ptrn4(4)
             
